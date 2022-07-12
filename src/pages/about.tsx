09b5d95a3a762fd6cab81b690440aa8e2eafd8aa
DESKTOP-RONIHRQ\Legion 5 <DuyetDS@fsoft.com.vn>
2022-07-12 17:56:29 +0700

    message

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -13,8 +13,13 @@ const About = () => (
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
       recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
       voluptatibus distinctio recusandae autem esse explicabo molestias officia
-      placeat, accusamus aut saepe.
+      placeat, accusamus aut saepe. xin chào các bạn mình là Duyệt
     </p>
+    <img
+      src="https://i.pinimg.com/736x/dc/a0/1a/dca01a9c483abacc1a6652787780f8e0.jpg"
+      width={400}
+      height={400}
+    />
   </Main>
 );
 
